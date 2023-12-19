9e2367e789591ea05a8d41c9ef1f3c95985ae2ed
L&H <abc@hub.moe>
2023-12-19 13:05:05 +0800

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,13 @@ module.exports = {
         worker: true,
         serviceworker: true,
     },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    plugins: ['@typescript-eslint'],
     rules: {
         '@typescript-eslint/ban-ts-comment': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
